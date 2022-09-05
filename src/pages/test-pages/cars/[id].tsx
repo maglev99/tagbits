cc2f3dc691903cd798138e13e869b8c403c4e087
Maglev <maglev@tagbits.xyz>
2022-09-04 18:02:38 -0700

    added test-pages directory for testing routes

---
+++ src/pages/test-pages/cars/[id].tsx
@@ -0,0 +1,10 @@
+import { Router, useRouter } from "next/router"
+
+export default function Car() {
+
+		const router = useRouter()
+
+		const { id } = router.query
+
+		return <h1>Hello {id}</h1>
+}
