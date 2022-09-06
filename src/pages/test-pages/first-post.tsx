be3f4bc9129547edf6eb8707d94a4d9f0ffa3fbe
Maglev <maglev@tagbits.xyz>
2022-09-05 22:07:31 -0700

    added docker files and some example files in pages to test next routing

--- src/pages/test-pages/first-post.tsx
+++ src/pages/test-pages/first-post.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
 
-const FirstPost = () => <h1>First Post</h1>
+const FirstPost = () => (
+  <>
+     <Head>
+        <title>First Post</title>
+      </Head>
+    <h1>
+      <Link href="/">Home</Link>
+    </h1>
+    <h1>
+      First Post, <Link href="/test-pages/second-post">Second Post</Link>
+    </h1>
+  </>
+)
 
 export default FirstPost
---
+++ src/pages/test-pages/second-post.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const SecondPost = () => <h1>Second Post</h1>
+
+export default SecondPost
