811bd28332ece19b99fa9365a88d4937d429dbeb
Maglev <maglev@tagbits.xyz>
2022-09-20 23:17:24 -0700

    changed graphql enpoint from using localhost:3000 URL to using relative path /api/graphql to account for URL path change when deploy on Vercel

--- graphql/useGQLQuery.ts
+++ graphql/useGQLQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import { GraphQLClient } from 'graphql-request'
 
 const useGQLQuery = (key: any, query: any, variables: any, config = {}) => {
-  const endpoint = 'http://localhost:3000/api/graphql'
+  const endpoint = '/api/graphql'
 
   const graphQLClient = new GraphQLClient(endpoint)
 
