c84e6a47ea9ac2df04ffdb3bdf352b250b7dcee2
Maglev <maglev@tagbits.xyz>
2022-11-17 01:20:06 -0800

    implemented mobile view for collector info row

--- src/pages/collectors/components/collectorInfo.tsx
+++ src/pages/collectors/components/collectorInfo.tsx
@@ -48,6 +48,44 @@ const ProfilePicture = ({
   )
 }
 
+const ProfilePictureMobile = ({
+  profilePic,
+  walletAddress,
+  onErrorFunction,
+}: any) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  const loaderStyle =
+    'bg-blue-700 after:block after:shadow-[0_0_150px_80px_rgba(254,254,254)] after:animate-[load_2.5s_infinite]' // after:animate-[load_1s_infinite]
+
+  const [visibility, setVisibility] = useState(`${loaderStyle}`)
+
+  const profilePicSize = 50
+
+  return (
+    <div
+      data-placeholder
+      className={`overflow-hidden mx-1 w-[${profilePicSize}px] h-[${profilePicSize}px] rounded-full ${visibility}`}
+    >
+      <a
+        href={`https://objkt.com/profile/${walletAddress}/owned`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FutureImage
+          src={profilePic}
+          alt="Profile Photo"
+          width={profilePicSize}
+          height={profilePicSize}
+          className={`w-[${profilePicSize}px] h-[${profilePicSize}px] object-cover`} // set invisible for debugging
+          onError={() => onErrorFunction()}
+          onLoadingComplete={() => setVisibility('')}
+        />
+      </a>
+    </div>
+  )
+}
+
 const TwitterIcon = ({ twitterURL, iconSize }: any) => (
   <div className={`ml-4 w-[${iconSize}px] h-[${iconSize}px]`}>
     <a href={twitterURL} target="_blank" rel="noopener noreferrer">
@@ -79,7 +117,6 @@ const CollectorInfo = ({ collector }: any) => {
     setProfilePic('')
   }
 
-  const profilePicSize = '60'
   const iconSize = '30'
 
   const defaultTzktProfilePic = `${'https://services.tzkt.io/v1/avatars/'}${walletAddress}`
@@ -87,33 +124,21 @@ const CollectorInfo = ({ collector }: any) => {
   return (
     <>
       {/* mobile view */}
-      {/* <a
-                  href={`https://objkt.com/explore/tokens/1?tags=${replaceLinkSymbols(
-                    filteredTag
-                  )}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={`"font-sans lg:font-light mx-2 px-2 py-1 rounded-xl my-2 text-[#F3F3F3] ${chooseTagColor(item?.count)} hover:underline max-w-sm truncate text-ellipsis"`}
-                >
-                  {filteredTag}
-                </a> */}
-
-      {/* tablet and desktop view */}
-      <div className={`${containerStyle} hidden md:flex`} key={rank}>
+      <div className={`${containerStyle} md:hidden`} key={rank}>
         <div
           className={`${centerContainerOnly} max-w-[960px] grow items-center bg-blue-200`}
         >
-          <h1 className={`${rowDataStyle} w-11`}>{rank}.</h1>
+          <h1 className={`${rowDataStyle} w-11 ml-4`}>{rank}.</h1>
           {profilePic !== null &&
           profilePic !== 'N/A' &&
           profilePic.slice(0, 21) === 'https://pbs.twimg.com' ? (
-            <ProfilePicture
+            <ProfilePictureMobile
               profilePic={profilePic}
+              walletAddress={walletAddress}
               onErrorFunction={clearImageLink}
             />
           ) : (
-            <ProfilePicture
-              profilePicSize={profilePicSize}
+            <ProfilePictureMobile
               profilePic={defaultTzktProfilePic}
               walletAddress={walletAddress}
               onErrorFunction={null}
@@ -126,12 +151,74 @@ const CollectorInfo = ({ collector }: any) => {
           >
             {/* if have alias display alias (nickname), else if have tzdomain display tzdomain, else display shortened address */}
             {nickname !== null && nickname.trim().length > 0 ? (
-              <h1 className={`${rowDataStyle} hover:underline`}>{nickname}</h1>
+              <h1 className={`${rowDataStyle} hover:underline ml-2`}>
+                {nickname}
+              </h1>
             ) : tzDomain !== null && tzDomain.trim().length > 0 ? (
-              <h1 className={`${rowDataStyle} hover:underline`}>{tzDomain}</h1>
+              <h1 className={`${rowDataStyle} hover:underline ml-2`}>
+                {tzDomain}
+              </h1>
             ) : (
               <h1
-                className={`${rowDataStyle} hover:underline`}
+                className={`${rowDataStyle} hover:underline ml-2`}
+              >{`${walletAddress.slice(0, 5)}...${walletAddress.slice(
+                -5
+              )}`}</h1>
+            )}
+          </a>
+        </div>
+      </div>
+      <div className={`${containerStyle} md:hidden`} key={rank}>
+        <div
+          className={`${centerContainerOnly} -mt-6 max-w-[960px] grow items-center bg-blue-200`}
+        >
+          <div className={`${rowDataStyle} ml-4 mr-1`}>
+            Volume: {(parseFloat(volume) / 1000000).toFixed(2)} XTZ
+          </div>
+					{twitter !== null && (
+            <TwitterIcon twitterURL={twitter} iconSize={iconSize} />
+          )}
+        </div>
+      </div>
+
+      {/* tablet and desktop view */}
+      <div className={`${containerStyle} hidden md:flex`} key={rank}>
+        <div
+          className={`${centerContainerOnly} max-w-[960px] grow items-center bg-blue-200`}
+        >
+          <h1 className={`${rowDataStyle} w-11`}>{rank}.</h1>
+          {profilePic !== null &&
+          profilePic !== 'N/A' &&
+          profilePic.slice(0, 21) === 'https://pbs.twimg.com' ? (
+            <ProfilePicture
+              profilePic={profilePic}
+              walletAddress={walletAddress}
+              onErrorFunction={clearImageLink}
+            />
+          ) : (
+            <ProfilePicture
+              profilePic={defaultTzktProfilePic}
+              walletAddress={walletAddress}
+              onErrorFunction={null}
+            />
+          )}
+          <a
+            href={`https://objkt.com/profile/${walletAddress}/owned`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {/* if have alias display alias (nickname), else if have tzdomain display tzdomain, else display shortened address */}
+            {nickname !== null && nickname.trim().length > 0 ? (
+              <h1 className={`${rowDataStyle} hover:underline ml-2`}>
+                {nickname}
+              </h1>
+            ) : tzDomain !== null && tzDomain.trim().length > 0 ? (
+              <h1 className={`${rowDataStyle} hover:underline ml-2`}>
+                {tzDomain}
+              </h1>
+            ) : (
+              <h1
+                className={`${rowDataStyle} hover:underline ml-2`}
               >{`${walletAddress.slice(0, 5)}...${walletAddress.slice(
                 -5
               )}`}</h1>
